06f6a029d3144b05c4c61fcca30be60187acc4fd
NeerajKhemani <neerajkhemani009@gmail.com>
2024-11-22 03:42:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Example JavaScript for form validation or interactive features
+document.getElementById('login-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Login Form Submitted!');
+  });
+  
+  document.getElementById('signup-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Sign Up Form Submitted!');
+  });
+  
